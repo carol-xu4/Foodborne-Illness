a6bc768485f9a1036dbb86be56e06be128cd0fc1
carol-xu4 <carol.xu@emory.edu>
2026-04-10 10:41:49 -0400

    meow

---
+++ data-code/cleaning.r
@@ -0,0 +1,7 @@
+## Preliminaries -----------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggplot2, ggthemes, dplyr, lubridate, stringr, readxl, data.table, gdata, readr)
+
+# Set working directory ----------------------------------------------------
+setwd("C:/Users/CarolXu/OneDrive - Cato Institute/Desktop/CPS ASEC")
+
