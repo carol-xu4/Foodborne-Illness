10ea7e7ad275f613ff770e15df2fd1a2416616c5
carol-xu4 <carol.xu@emory.edu>
2026-04-14 15:58:10 -0400

    ૮ ˶ᵔ ᵕ ᵔ˶ ა

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -7,17 +7,9 @@ setwd("C:/Users/CarolXu/OneDrive - Cato Institute/Desktop/foodborne illness")
 
 # read in new data ---------------------------------------------------------
 data = read.csv("data/output/foodborne_data.csv")
+records = paste0("record_", 1:20)
 
 # pathogen counts
-other = c("A05", "A059")
-campylobacter = "A045"
-listeria = c("A32", "A327")
-salmonella = c("A02", "A020", "A021", "A029")
-stec = "A043"
-yersinia = "A046"
-
-illness = c(other, campylobacter, listeria, salmonella, stec, yersinia)
-
 icd_year_counts = data %>%
   mutate(
     A05  = if_any(c(ucod, all_of(records)), ~ .x == "A05"),
@@ -49,3 +41,59 @@ icd_year_counts = data %>%
     A046 = sum(A046, na.rm = TRUE))
 
 write_csv(icd_year_counts, "results/icd_year_counts.csv")
+
+other = c("A05", "A059")
+campylobacter = "A045"
+listeria = c("A32", "A327")
+salmonella = c("A02", "A020", "A021", "A029")
+stec = "A043"
+yersinia = "A046"
+
+pathogen_counts = data %>%
+  mutate(
+    Other = if_any(c(ucod, all_of(records)), ~ .x %in% other),
+    Campylobacter = if_any(c(ucod, all_of(records)), ~ .x %in% campylobacter),
+    Listeria = if_any(c(ucod, all_of(records)), ~ .x %in% listeria),
+    Salmonella = if_any(c(ucod, all_of(records)), ~ .x %in% salmonella),
+    STEC = if_any(c(ucod, all_of(records)), ~ .x %in% stec),
+    Yersinia = if_any(c(ucod, all_of(records)), ~ .x %in% yersinia)) %>%
+  group_by(year) %>%
+  summarise(
+    Other = sum(Other, na.rm = TRUE),
+    Campylobacter = sum(Campylobacter, na.rm = TRUE),
+    Listeria = sum(Listeria, na.rm = TRUE),
+    Salmonella = sum(Salmonella, na.rm = TRUE),
+    STEC = sum(STEC, na.rm = TRUE),
+    Yersinia = sum(Yersinia, na.rm = TRUE),
+    .groups = "drop")
+
+write_csv(pathogen_counts, "results/pathogen_year_counts")
+
+count_data = pathogen_counts %>%
+  pivot_longer(
+    cols = -year,
+    names_to = "pathogen",
+    values_to = "n")
+
+ggplot(count_data, aes(x = as.numeric(year), y = n, color = pathogen)) +
+  geom_line(linewidth = 1.1) +
+  geom_point() +
+  scale_y_continuous(breaks = seq(0, 120, by = 10)) +
+  labs(
+    title = "Foodborne Illness Deaths by Pathogen (1996-2020)",
+    subtitle = "CDC NVSS Mortality Multiple Cause-of-Death data (NBER)",
+    x = "Year",
+    y = "Number of Deaths",
+  ) +
+  theme_stata() +
+  theme(plot.title = element_text(size = 40, face = "bold", hjust = 0, color = "black"),
+        plot.subtitle = element_text(size = 30, color = "black", margin = margin(b = 12), hjust = 0),
+        legend.position = "bottom",
+        legend.text = element_text(size = 20),
+        axis.title.y = element_text(size = 30),
+        axis.title.x = element_text(size = 30),
+        axis.text.x = element_text(size = 35), 
+        axis.text.y = element_text(size = 35, angle = 0, vjust = 0.5),
+        plot.caption = element_text(size = 12),
+        plot.background = element_rect(fill = "white"))
+ggsave("results/deaths_by_pathogen.png", width = 20, height = 15)
